b03b7879ff4959a6d1f123e53c0860d2d10ce232
anraku <anraku90@gmail.com>
2019-10-19 20:00:11 +0900

    apiリクエストパスの記述を変更

--- front/pages/users/_id.vue
+++ front/pages/users/_id.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
   asyncData({ $axios, params }) {
-    return $axios.$get(`http://back:3000/users/${params.id}`).then((res) => {
+    return $axios.$get(`/api/users/${params.id}`).then((res) => {
       return { name: res.name }
     })
   }
